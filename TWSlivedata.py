bee4ab01cb49883ecc8e280917e38e26ce4d73dc
Vignesh Mani <vigneshms565@gmail.com>
2023-07-08 01:14:01 +0800

    Update TWSlivedata.py

--- TWSlivedata.py
+++ TWSlivedata.py
@@ -14,17 +14,12 @@ bars = ib.reqHistoricalData(
 data = pd.DataFrame(bars)
 
 #code to get real time data
-
 mkt_data = ib.reqMktData(contract, '', False, False)
-# df_mktdata = util.df(mkt_data)
 
 ib.sleep(2)
 
-# print(df_mktdata)
-# print(mkt_data)
-
 def onPendingTicker(tickers):
-    print("pending ticker event received")
+    print("Pending ticker event received: ")
     print(tickers)
 
 ib.pendingTickersEvent += onPendingTicker
